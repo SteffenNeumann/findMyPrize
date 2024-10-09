e359688930dfe8fa3a87c3aa71a04a03aa57e03b
Mastercaution <neumann-steffen@web.de>
2024-10-09 08:18:49 +0200

    replace twilio by mail

--- Main.py
+++ Main.py
@@ -1,41 +1,50 @@
 from playwright.sync_api import sync_playwright
-from twilio.rest import Client
 import os
 from geopy.geocoders import Nominatim
 from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
 
 geolocator = Nominatim(user_agent="my_user_agent")
 city ="Erding"
 country ="Germany"
-product = "Red Bull"
+product = "Joghurt"
 
 loc = geolocator.geocode(city+','+ country)
 my_long=loc.longitude
 my_lat=loc.latitude
 
-env_file_path = './twilio.env'
+load_dotenv()
 
-if os.path.exists(env_file_path):
-    with open(env_file_path, 'r') as f:
-        for line in f:
-            key, value = line.strip().split('=')
-            os.environ[key] = value
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL')
 
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-RECIPIENT_PHONE_NUMBER = '+491735159382'
 
-def send_sms(message):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    message = client.messages.create(
-        body=message,
-        from_=TWILIO_PHONE_NUMBER,
-        to=RECIPIENT_PHONE_NUMBER
-    )
+def send_email(subject, message):
+    sender_email = EMAIL_ADDRESS
+    sender_password = EMAIL_PASSWORD
+    receiver_email = RECIPIENT_EMAIL
+    
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    text = msg.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
+
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(headless=False)
     context = browser.new_context(viewport={"width": 1920, "height": 1080})
     page = context.new_page()
 
@@ -44,16 +53,16 @@ with sync_playwright() as p:
     page.goto(url)
     page.wait_for_load_state('load', timeout=20000)
 
-    offer_section = page.query_selector('section[data-testid="OfferGrid"]')
+    offer_section = page.wait_for_selector('.search-group-grid-content', timeout=30000)
 
     if not offer_section:
         output = f"No Product {product} found"
     else:
-        products = offer_section.query_selector_all('div.flex.cursor-pointer.flex-col.justify-between.rounded-lg.border.border-gray.bg-white.text-dark')
+        products = offer_section.query_selector_all('.card.card--offer.slider-preventClick')
         output = ""
         for product in products:
-            store_element = product.query_selector('.truncate.text-sm.text-dark1')
-            price_element = product.query_selector('.text-md.font-bold.text-primary')
+            store_element = product.query_selector('.card__subtitle')
+            price_element = product.query_selector('.card__prices-main-price')
 
             if store_element and price_element:
                 store = store_element.inner_text().strip()
@@ -61,10 +70,16 @@ with sync_playwright() as p:
 
                 try:
                     price_value = float(price_text.replace('€', '').replace(',', '.').strip())
-
                     if price_value < 0.99:
-                        message = f"Deal alert! {store} offers Red Bull for only {price_text}!"
-                        send_sms(message)
+                        subject = "Deal Alert!"
+                        product_name_element = product.query_selector('.card__title')
+                        if product_name_element:
+                            product_name = product_name_element.inner_text().strip()
+                        else:
+                            product_name = "Unknown Product"
+                        
+                        message = f"Deal alert! {store} offers {product_name} for only {price_text}!"
+                        send_email(subject, message)
                         output += message + "\n"
                 except ValueError:
                     print(f"Could not convert price to float: {price_text}")
